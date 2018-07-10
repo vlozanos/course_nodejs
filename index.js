920dfcbc811ecb7f59ef6ad64987579ee1518247
vitor <vic_tor1221@hotmail.com>
2018-07-09 20:06:17 -0600

    add branch developer

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = require('./app');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/zoo')
     .then(()=>{
-        console.log('La conexion a mongo a sido exitosa');
+        console.log('La conexion a mongo a sido exitosas');
         app.listen(port,()=>{
         console.log('El servidor local de node y express esta corriendo');
     });
